c58ffbe5e8f84807b33266df72b03559b2d66aaa
Valeria <vdelaconcep@gmail.com>
2025-09-02 23:27:11 -0300

    Primera presentación

--- src/components/ubicacion.jsx
+++ src/components/ubicacion.jsx
@@ -11,7 +11,7 @@ const Ubicacion = () => {
                     transition={{ duration: 0.8 }}
                     whileInView={{ y: 0, opacity: 1 }}
                     viewport={{ once: true, amount: 0.4 }}
-                    className="md:w-full"
+                    className="w-full"
                 >
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3280.7535495728057!2d-58.4176390334281!3d-34.68616902118912!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccc60204be501%3A0xb24f60cfb640a203!2sViamonte%202615%2C%20B1824%20Valent%C3%ADn%20Alsina%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses!2sar!4v1756140255680!5m2!1ses!2sar" className="rounded-lg h-[200px] md:h-[464px] w-full shadow-sm shadow-gray-900" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                 </motion.div>
