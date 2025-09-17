e4651349507acb2de5aee6e33c28805d7a246f14
vdelaconcep <vdelaconcep@gmail.com>
2025-09-17 18:23:53 -0300

    Resolviendo problemas

--- Back/api/index.js
+++ Back/api/index.js
@@ -2,4 +2,8 @@ import app from '../app.js';
 
 import serverless from "serverless-http";
 
+console.log("✅ Imports cargados correctamente");
+console.log("✅ App:", typeof app);
+console.log("✅ Serverless:", typeof serverless);
+
 export default serverless(app);
