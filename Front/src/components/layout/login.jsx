d2943a76f178ff59151252e042f5c608b7164f19
vdelaconcep <vdelaconcep@gmail.com>
2025-09-17 16:31:57 -0300

    Actualizado para deploy en vercel

--- Front/src/components/layout/login.jsx
+++ Front/src/components/layout/login.jsx
@@ -20,7 +20,7 @@ const Login = () => {
 
     return (
         <section>
-            <article className="flex mt-1 justify-end text-xs md:text-s lg:text-sm">
+            <article className="flex pt-1 justify-end text-xs md:text-s lg:text-sm">
                 <button
                     className="border-none text-red-200 hover:underline cursor-pointer"
                     onClick={() => setAbrirModal(true)}>
