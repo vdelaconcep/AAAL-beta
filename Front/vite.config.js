03269e035d86c55591b3456647171b1da804c31e
vdelaconcep <vdelaconcep@gmail.com>
2025-09-18 16:20:34 -0300

    Solucionando problemas de deploy

--- Front/vite.config.js
+++ Front/vite.config.js
@@ -1,11 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    // Para definir rutas absolutas en las importaciones de archivos
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+
   plugins: [
     react(),
     tailwindcss()
-  ]
+  ],
+
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    historyApiFallback: true
+  },
+  preview: {
+    historyApiFallback: true
+  }
 });
