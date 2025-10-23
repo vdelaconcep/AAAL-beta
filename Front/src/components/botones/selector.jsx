29e9fb387e6aeaf86cbcef0f2aa5537c63f503f5
Valeria <vdelaconcep@gmail.com>
2025-10-22 22:46:57 -0300

    Trabajando sobre diseño de galería de fotos

--- Front/src/components/botones/selector.jsx
+++ Front/src/components/botones/selector.jsx
@@ -1,7 +1,7 @@
 const Selector = ({ tipo, texto, accion, clase, seleccionado }) => {
     return (
         <button
-            className={`border-b-[4px] ${seleccionado ? 'border-b-[#6B9795]' : 'border-b-transparent'} bg-gray-100 hover:bg-gray-200 active:bg-gray-200 font-medium cursor-pointer py-2 ${clase && clase}`}
+            className={`border-b-[4px] ${seleccionado ? 'border-b-[#6B9795]' : 'border-b-transparent'} bg-[#fff4da] hover:bg-[#ffeab9] active:bg-[#ffeab9] font-medium cursor-pointer py-2 ${clase && clase}`}
             type={tipo}
             onClick={accion && accion}
         >
--- Front/src/components/galeria/eventoCard.jsx
+++ Front/src/components/galeria/eventoCard.jsx
@@ -1,4 +1,5 @@
 import formatearUTC from "@/utils/formatearUTC";
+import BotonSecundario from '@/components/botones/secundario'
 
 const EventoCard = ({ dato, onClick }) => {
     const fechaCompleta = formatearUTC(dato.fecha);
@@ -7,17 +8,25 @@ const EventoCard = ({ dato, onClick }) => {
     return (
         <article
             onClick={onClick}
-            className="w-full h-full bg-gray-200 hover:bg-gray-300 overflow-hidden rounded-xl shadow-sm shadow-gray-800 cursor-pointer hover:shadow-md">
-            <div className="bg-gray-800 w-full overflow-hidden">
+            className="w-full h-full bg-white overflow-hidden rounded-xl shadow-sm shadow-gray-800 cursor-pointer hover:shadow-md group">
+            <div className="bg-gray-800 w-full overflow-hidden group">
                 <img
-                    className="w-full h-full object-contain aspect-[536/354]"
+                    className="w-full h-full object-contain aspect-[536/354] group-hover:brightness-105 transition-all duration-300 ease-in-out"
                     src={dato.foto_url}
                     alt={dato.nombre} />
             </div>
-            <div className="p-3">
-                <h1 className="font-semibold">{dato.nombre}</h1>
-                <h5 className="text-gray-500">{fechaCorta}</h5>
-                <h4 className="text-gray-700">{dato.descripcion}</h4>
+            <div className="pt-3 pb-2 ">
+                <h1 className="px-3 font-bold">{dato.nombre}</h1>
+                <h4 className="text-gray-700 leading-5 px-3 pb-3">{dato.descripcion}</h4>
+                <hr />
+                <div className="px-3 pt-2 flex justify-between h-[30px]">
+                    <h5 className="text-gray-500">{fechaCorta}</h5>
+                    <BotonSecundario
+                        texto='ver'
+                    clase='py-0 rounded-[5px]'/>
+                </div>
+                
+                
             </div>
             
         </article>
